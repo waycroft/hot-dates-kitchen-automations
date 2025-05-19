e6216ae36ec491368a2ac81089bcc5dc467426ed
Tyler Termini <ttermini@adobe.com>
2025-05-18 22:40:04 -0600

    add docs

--- services/hot-dates-kitchen-automations/packages/hot-dates-kitchen-auto-shipping-labels/src/index.js
+++ services/hot-dates-kitchen-automations/packages/hot-dates-kitchen-auto-shipping-labels/src/index.js
@@ -39,40 +39,8 @@ async function main(request, env, ctx) {
 			//if (fulfillmentOrder.status === "CLOSED") {
 			//	continue
 			//}
-			console.log(JSON.stringify(fulfillmentOrder, null, 2));
+			//console.log(JSON.stringify(fulfillmentOrder, null, 2));
 
-			// Sample "Create shipment" request body
-			// {
-			// 	"shipment": {
-			// 	  "to_address": {
-			// 		"name": "Dr. Steve Brule",
-			// 		"street1": "179 N Harbor Dr",
-			// 		"city": "Redondo Beach",
-			// 		"state": "CA",
-			// 		"zip": "90277",
-			// 		"country": "US",
-			// 		"phone": "8573875756",
-			// 		"email": "dr_steve_brule@gmail.com"
-			// 	  },
-			// 	  "from_address": {
-			// 		"name": "EasyPost",
-			// 		"street1": "417 Montgomery Street",
-			// 		"street2": "5th Floor",
-			// 		"city": "San Francisco",
-			// 		"state": "CA",
-			// 		"zip": "94104",
-			// 		"country": "US",
-			// 		"phone": "4153334445",
-			// 		"email": "support@easypost.com"
-			// 	  },
-			// 	  "parcel": {
-			// 		"length": "20.2",
-			// 		"width": "10.9",
-			// 		"height": "5",
-			// 		"weight": "65.9"
-			// 	  }
-			// 	}
-			//   }
 			const shipment = {
 				from_address: {
 					name: 'Hauser',
@@ -97,6 +65,7 @@ async function main(request, env, ctx) {
 				},
 				parcel: {
 					// Easypost parcel always requires weight in ounces.
+					// TODO: Are dimensions required?
 					mode: 'test',
 					weight: fulfillmentOrder.lineItems.nodes.reduce((acc, item) => {
 						if (item.weight.unit === "POUNDS") {
@@ -107,7 +76,7 @@ async function main(request, env, ctx) {
 					}, 0),
 				},
 			}
-			console.log('Shipment:\n' + JSON.stringify(shipment, null, 2));
+			//console.log('Shipment:\n' + JSON.stringify(shipment, null, 2));
 
 			// Create shipment
 			const shipmentResponse = await easypost.createShipment(shipment);
